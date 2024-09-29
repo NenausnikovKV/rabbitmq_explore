19e7cfb5a4b72f8da7d61816005b83a89c592156
Konstantin Nenausnikov <konstantin2113@mail.ru>
2024-09-29 16:15:08 +0300

    first app  - Hello world

---
+++ hello_world/receive.py
@@ -0,0 +1,37 @@
+"""
+Simple receiver of rabbitMQ queue
+"""
+import os
+import sys
+
+import pika
+
+
+def consumer_process():
+    """
+    Create connection with localhost standard port 5672
+    Create or connect to chanel "hello"
+    listen chanel and call callback function if channel will have message.
+    """
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='hello')
+
+    def callback(ch, method, properties, body):
+        print(f" [x] Received {body}")
+
+    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+
+if __name__ == '__main__':
+    try:
+        consumer_process()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
